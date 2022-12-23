40f5a44c17ab78088e21560828efc5ad738395d9
Kathiriya Sharmil <90636087+kathiriyasharmil@users.noreply.github.com>
2022-12-23 20:39:50 +0530

    add  new file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
+import React from "react";
 import logo from './logo.svg';
 import './App.css';
+import Check from "./check";
 
 function App() {
   return (
     <div className="App">
-      my name is sharmil
+        my name is sharmil
         my name is sharmil
         my name is sharmil
         my name is sharmilv
+        my name is sharmil
+        my name is sharmil
+        my name is sharmil
         my name is sharmil
         master
       my name is sharmil
@@ -23,6 +28,7 @@ function App() {
         my name is sharmil
         my name is sharmil
         master
+        <Check/>
     </div>
   );
 }
