aa364fef6b964b5d20823894dfe5fa0300669d99
Kathiriya Sharmil <90636087+kathiriyasharmil@users.noreply.github.com>
2022-12-23 21:25:05 +0530

    CreateFromMaster

--- src/App.js
+++ src/App.js
@@ -1,30 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import Check from "./Check"
 
 function App() {
-  return (
-    <div className="App">
-      my name is sharmil
-        my name is sharmil
-        my name is sharmil
-        my name is sharmilv
-        my name is sharmil
-        master
-      my name is sharmil
-      my name is sharmil
-      my name is sharmilv
-      my name is sharmil
-     sam sam
-        my name is sharmil
-        my name is sharmil
-        my name is sharmil
-        my name is sharmilv
-        my name is sharmil
-        my name is sharmil
-        my name is sharmil
-        master
-    </div>
-  );
+    return (
+        <div className="App">
+            my name is sharmil
+            my name is sharmil
+            my name is sharmil
+            my name is sharmilv
+            my name is sharmil
+            master
+            my name is sharmil
+            my name is sharmil
+            my name is sharmilv
+            my name is sharmil
+            sam sam
+            my name is sharmil
+            my name is sharmil
+            my name is sharmil
+            my name is sharmilv
+            my name is sharmil
+            my name is sharmil
+            my name is sharmil
+            master
+            <Check/>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Check.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Check = () => {
+    return (
+        <div>
+            Check createfrommaster
+        </div>
+    );
+};
+
+export default Check;
